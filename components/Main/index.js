8caa95b3d9878fcf1e912c744438bcf9c2f677bc
Manav Misra <manavm@visionwebsoft.com>
2021-02-11 07:49:05 -0600

    :construction: Render clickable board or ask for names

--- components/Main/index.js
+++ components/Main/index.js
@@ -1,3 +1,9 @@
 import Board from "./Board";
+import Input from "./NameInput";
 
-export default ({ board }) => `<main>${Board(board)}</main>`;
+function ask4Name([p1]) {
+  return p1 ? Input("Player 2") : Input("Player 1");
+}
+
+export default ({ board, names }) =>
+  `<main>${names.length === 2 ? Board(board) : ask4Name(names)}</main>`;
--- index.js
+++ index.js
@@ -4,14 +4,28 @@ const root = document.getElementById("root");
 
 const state = {
   board: Array.from({ length: 9 }, (_, i) => i),
-  player1Name: "",
-  player2Name: "",
+  names: [],
   currentPlayer: null,
   winner: null,
 };
 
 const render = (st = state) => {
   root.innerHTML = Main(st);
+
+  if (st.names.length < 2) {
+    root.querySelector("form").addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      state.names = [...st.names, e.target.elements[0].value];
+      render();
+    });
+  } else {
+    document.querySelectorAll("div[data-num]").forEach((square) => {
+      square.addEventListener("click", function handleClick() {
+        console.log(this);
+      });
+    });
+  }
 };
 
 render();
