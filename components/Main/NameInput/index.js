2e3bbb09c61d3e0643c832ace70ecb5ab0cd385b
Manav Misra <manavm@visionwebsoft.com>
2021-02-11 07:48:30 -0600

    :building_construction: Ren directory for inputs

---
+++ components/Main/NameInput/index.js
@@ -0,0 +1,2 @@
+export default (currentPlayer) =>
+  `<form><label for="player-name">${currentPlayer}, What is your good name?</label><input type="text" id="player-name" /><button>Submit!</button></form>`;
